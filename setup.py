54029b438b25b2c5b312df74470847dd39629a9d
javapagar <apariciogarcia.j@gmail.com>
2021-05-06 16:06:53 +0200

    version 2

--- setup.py
+++ setup.py
@@ -2,22 +2,22 @@ from distutils.core import setup
 setup(
   name = 'libraryname',         # How you named your package folder (MyLib)
   packages = ['libraryname'],   # Chose the same as "name"
-  version = 'v_01',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'ds-dt-e21',                   # Type in your name
   author_email = 'captainvinager@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/javapagar/pip_install_clase',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/javapagar/pip_install_clase/archive/refs/tags/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/javapagar/pip_install_clase/archive/refs/tags/v_02.tar.gz',    # I explain this later on
   keywords = ['Machine Learning', 'Easy', 'Help','Visualization','Clean'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
           'numpy',
           'seaborn',
           'matplotlib',
-          'plotlyb',
+          'plotly',
           'seaborn',
-          'plotlyb.offline',
+          'plotly.offline',
           'scipy',
           'sklearn'
 
